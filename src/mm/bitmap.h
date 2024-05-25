600345364439f1383ca586a4fb17b04983547567
xrc2 <109512837+9xbt@users.noreply.github.com>
2024-05-25 20:28:19 +0200

    Update bitmap.h

--- src/mm/bitmap.h
+++ src/mm/bitmap.h
@@ -1,8 +1,3 @@
-/*
- * CREDITS: astrido
- * https://github.com/asterd-og/ZanOS/
- */
-
 #pragma once
 
 #include <types.h>
@@ -16,5 +11,5 @@ void bitmap_clear(u8* bitmap, u64 bit) {
 }
 
 bool bitmap_get(u8* bitmap, u64 bit) {
-    return (bitmap[bit / 8] & (1 << (bit % 8))) != 0;
+    return bitmap[bit / 8] & (1 << (bit % 8));
 }
