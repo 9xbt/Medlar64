0d2810c0e13ca067cf04a04d5834b157b3c0234b
xrc2 <109512837+9xbt@users.noreply.github.com>
2024-05-25 16:36:51 +0200

    Add code for detecting page count

--- src/mm/pmm.c
+++ src/mm/pmm.c
@@ -15,9 +15,21 @@ void pmm_init() {
     pmm_memmap = memmap_request.response;
     struct limine_memmap_entry** entries = pmm_memmap->entries;
 
+    u64 higher_address;
+    u64 top_address;
+
     dprintf("pmm_init(): entry count: %d\n", pmm_memmap->entry_count);
 
     for (u64 i = 0; i < pmm_memmap->entry_count; i++) {
-        
+        if (entries[i]->type != LIMINE_MEMMAP_USABLE)
+            continue; // skip unusable memmaps
+
+        top_address = entries[i]->base + entries[i]->length;
+        if (top_address > higher_address)
+            higher_address = top_address;
     }
+
+    pmm_total_pages = higher_address / PAGE_SIZE;
+
+    dprintf("pmm_init(): total pages: %d\n", pmm_total_pages);
 }
