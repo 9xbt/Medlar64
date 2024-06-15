828b6c64665b38d559a7178cb913ec8a7eb4604f
xrc2 <109512837+9xbt@users.noreply.github.com>
2024-06-15 12:11:00 +0200

    ioapic: finish init function

--- src/dev/ioapic.c
+++ src/dev/ioapic.c
@@ -1,3 +1,4 @@
+#include <dev/char/serial.h>
 #include <dev/ioapic.h>
 #include <acpi/madt.h>
 #include <mm/pmm.h>
@@ -24,8 +25,9 @@ __attribute__((no_sanitize("undefined")))
 void ioapic_init() {
     madt_ioapic* ioapic = madt_ioapic_list[0];
 
-    u32 ver = ioapic_read(ioapic, IOAPIC_VER);
     u32 id = ioapic_read(ioapic, IOAPIC_ID) >> 24;
+    u32 ver = ioapic_read(ioapic, IOAPIC_VER);
+    u32 count = ioapic_gsi_count(ioapic);
 
     if (id != ioapic->id) {
         dprintf("ioapic: apic id mismatch\n");
@@ -33,5 +35,10 @@ void ioapic_init() {
         return;
     }
 
-    dprintf("ioapic: initialized ioapic\n");
+    for (u32 i = 0; i <= count; i++) {
+        ioapic_write(ioapic, IOAPIC_REDTBL + (2 * i), 0x10000 | (i + 32)); /* low 32 bits - mask the interrupt */
+        ioapic_write(ioapic, IOAPIC_REDTBL + (2 * i) + 1, 0); /* high 32 bits - redirect to cpu 0 */
+    }
+
+    dprintf("ioapic: initialized ioapic with %d interrupts\n", count + 1);
 }
--- src/dev/ioapic.h
+++ src/dev/ioapic.h
@@ -1,8 +1,9 @@
 #pragma once
 
-#define IOAPIC_ID  0x0
-#define IOAPIC_VER 0x1
-#define IOAPIC_ARB 0x2
+#define IOAPIC_ID     0x0
+#define IOAPIC_VER    0x1
+#define IOAPIC_ARB    0x2
+#define IOAPIC_REDTBL 0x10
 
 #define IOAPIC_REGSEL 0x0
 #define IOAPIC_IOWIN  0x10
